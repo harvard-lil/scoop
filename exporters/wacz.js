1d49ec1c6af98982e8e0a2a53f5174bc96612449
Greg Leppert <greg@leppert.me>
2022-10-09 17:45:48 +0200

    Initial generation of wacz in JS

--- exporters/wacz.js
+++ exporters/wacz.js
@@ -1,8 +1,7 @@
-import * as path from "path";
-import { mkdtemp, readFile, writeFile, rm } from "fs/promises";
-import { tmpdir } from "os";
+import { createWriteStream } from "fs";
 import { v4 as uuidv4 } from "uuid";
-import { spawn  } from "child_process";
+import { Writable } from "stream";
+import Archiver from "archiver";
 
 import { Mischief } from "../Mischief.js";
 import * as exporters from "../exporters/index.js";
@@ -16,48 +15,33 @@ import * as exporters from "../exporters/index.js";
  * @param {Mischief} capture
  * @returns {Promise<ArrayBuffer>}
  */
-export async function wacz(capture) {
-  const validStates = [Mischief.states.PARTIAL, Mischief.states.COMPLETE];
-  if (!(capture instanceof Mischief) || !validStates.includes(capture.state)) {
-    throw new Error("`capture` must be a partial or complete Mischief object.");
-  }
 
-  let tmpDir;
-  try {
-    tmpDir = await mkdtemp(path.join(tmpdir(), 'mischief'));
-    const pagesFile = path.join(tmpDir, 'pages.jsonl')
-    const warcFile = path.join(tmpDir, 'warc.warc');
-    const waczFile = path.join(tmpDir, 'wacz.wacz');
-
-    const pagesData = [{"format": "json-pages-1.0", "id": "pages", "title": "All Pages", hasText: false},
-                       {id: uuidv4(), url: capture.url, title: "", seed: true}].map(JSON.stringify).join('\n');
-    await writeFile(pagesFile, pagesData);
-
-    const warc = await exporters.warc(capture);
-    await writeFile(warcFile, Buffer.from(warc));
-
-    const createArgs = ["create", "--split-seeds", "-o", waczFile, "--pages", pagesFile, warcFile];
-    await awaitProcess(spawn("wacz" , createArgs, {stdio: "inherit"}));
-
-    return await readFile(waczFile);
-  }
-  catch(e) {
-    console.error(e);
-  }
-  finally {
-    try {
-      if (tmpDir) {
-        await rm(tmpDir, { recursive: true });
-      }
+export function wacz(capture) {
+  return new Promise((resolve, reject) => {
+    const validStates = [Mischief.states.PARTIAL, Mischief.states.COMPLETE];
+    if (!(capture instanceof Mischief) || !validStates.includes(capture.state)) {
+      throw new Error("`capture` must be a partial or complete Mischief object.");
     }
-    catch (e) {
-      console.error(`An error has occurred while removing the temp folder at ${tmpDir}. Please remove it manually. Error: ${e}`);
-    }
-  }
-};
 
-const awaitProcess = (proc) => {
-  return new Promise((resolve) => {
-    proc.on("close", (code) => resolve(code));
+    const buffers = [];
+    const converter = new Writable();
+    converter._write = (chunk, encoding, cb) => {
+      buffers.push(chunk)
+      process.nextTick(cb)
+    }
+    converter.on('finish', () => { resolve(Buffer.concat(buffers)) })
+
+    const archive = new Archiver('zip', { zlib: { level: 9 } });
+    archive.on('error', err => { reject(err) });
+    archive.pipe(converter);
+
+    const pages = [{"format": "json-pages-1.0", "id": "pages", "title": "All Pages", hasText: false},
+                   {id: uuidv4(), url: capture.url, title: "", seed: true}].map(JSON.stringify).join('\n');
+    archive.append(pages, {name: 'pages/pages.jsonl'});
+
+    exporters.warc(capture).then((warc) => {
+      archive.append(Buffer.from(warc), {name: 'archive/archive.warc'});
+      archive.finalize();
+    });
   });
 };
