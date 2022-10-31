83915bccd2684d88128d0aa7a301f2936d49e007
Greg Leppert <greg@leppert.me>
2022-10-31 14:14:30 -0400

    Import a list of exchanges from a wacz with raw

--- exporters/raw.js
+++ exporters/raw.js
@@ -1,34 +1,14 @@
-import { Writable } from "stream";
-import Archiver from "archiver";
-
-import { Mischief } from "../Mischief.js";
+import * as exporters from "../exporters/index.js";
 
 export async function raw(capture) {
-  const validStates = [Mischief.states.PARTIAL, Mischief.states.COMPLETE];
-  if (!(capture instanceof Mischief) || !validStates.includes(capture.state)) {
-    throw new Error("`capture` must be a partial or complete Mischief object.");
-  }
+  const files = capture.exchanges.map((exchange) => {
+    return ['request', 'response'].map((type) => {
+      return {
+        path: `raw/${type}_${exchange.date.toISOString()}_${exchange.id}`,
+        data: exchange[`${type}Raw`]
+      }
+    })
+  }).flat().filter(ex => ex.data);
 
-  const archive = new Archiver('zip', {store: true});
-
-  const buffers = [];
-  const converter = new Writable();
-  converter._write = (chunk, _encoding, cb) => {
-    buffers.push(chunk);
-    process.nextTick(cb);
-  }
-  archive.pipe(converter);
-
-  for (const exchange of capture.exchanges) {
-    for (const type of ['request', 'response']) {
-      if(!exchange[`${type}Raw`]) { continue; }
-      archive.append(
-        exchange[`${type}Raw`],
-        {name: `${type}_${exchange.date.toISOString()}_${exchange.id}`}
-      );
-    }
-  }
-
-  await archive.finalize();
-  return Buffer.concat(buffers);
+  return await exporters.wacz(capture, files);
 }
---
+++ importers/index.js
@@ -0,0 +1,11 @@
+/**
+ * Mischief
+ * @module importers
+ * @author The Harvard Library Innovation Lab
+ * @license MIT
+ * @description Entry point for the importers module.
+ */
+import { wacz } from "./wacz.js";
+export {
+  wacz
+};
