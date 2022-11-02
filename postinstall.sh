21e3598a1e8d0e58bfe64c4627d9aaf6bb91c997
Matteo Cargnelutti <mcargnelutti@law.harvard.edu>
2022-11-02 17:45:23 -0400

    package.json fix

--- postinstall.sh
+++ postinstall.sh
@@ -2,9 +2,6 @@
 # Post-install script
 #-------------------------------------------------------------------------------
 
-# System-wide Playwright install / update of Playwright
-npm i -D playwright;
-
 # Pull yt-dlp (2022.10.04 version)
 mkdir ./node_modules/yt-dlp/;
 curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2022.10.04/yt-dlp > ./node_modules/yt-dlp/yt-dlp;
