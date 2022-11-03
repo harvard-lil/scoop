e89a2bc3815b28a0165891645e2b1b47e25ddaea
Greg Leppert <greg@leppert.me>
2022-11-03 15:56:03 -0400

    Add comments

--- utils/WACZ.js
+++ utils/WACZ.js
@@ -1,11 +1,18 @@
+/**
+ * Mischief
+ * @module utils.WACZ
+ * @author The Harvard Library Innovation Lab
+ * @license MIT
+ * @description WACZ builder.
+ */
+
 import path from "path";
-import * as zip from "../utils/zip.js";
-
-import CONSTANTS from "../constants.js";
-
 import { Readable, Writable } from "stream";
-import { CDXIndexer } from "warcio";
 import { createHash } from "crypto";
+
+import { CDXIndexer } from "warcio";
+import CONSTANTS from "../constants.js";
+import * as zip from "../utils/zip.js";
 
 export class WACZ {
   validations = [
@@ -14,9 +21,18 @@ export class WACZ {
     [/^pages\//, [this.assertPages]]
   ]
 
+/**
+ * Checks to see if a buffer contains WARC data
+ * using simple sniff tests that are imperfect but
+ * helpful nonetheless.
+ *
+ * @param {string} fpath
+ * @param {Buffer} fdata
+ */
   assertWarc(fpath, fdata) {
     buf = Buffer.from(fdata);
 
+    // If the first 4 bytes are "WARC", let's assume it's an uncompressed WARC
     if(buf.toString('utf8', 0, 4) === 'WARC'){
         return;
     }
@@ -31,47 +47,57 @@ export class WACZ {
       }
     }
 
+    // None of the WARC sniff tests passed so throw a final error
     throw `${fpath}: must be a valid WARC file`;
   }
 
+/**
+ * Checks to see if a buffer contains CDXJ data
+ * using simple sniff tests that are imperfect but
+ * helpful nonetheless.
+ *
+ * @param {string} fpath
+ * @param {Buffer} fdata
+ */
   assertIndex(fpath, fdata) {
-    const json = JSON.parse(Buffer.from(fdata).toString().split('\n')[0].split(/ \d{14} /)[1])
+    // CDXJ's are newline delineated
+    const lines = Buffer.from(fdata).toString().split('\n');
+    // Grab the JSON blob from the first line and parse it
+    const json = JSON.parse(lines[0].split(/ \d{14} /)[1]);
+    // Use the url property of the JSON as a sniff test
     if(!json.url){
       throw `${fpath}: must be a valid CDXJ file`;
     }
   }
 
+/**
+ * Checks to see if a buffer contains Pages data
+ * in JSON-Lines format using simple sniff tests
+ * that are imperfect but helpful nonetheless.
+ *
+ * @param {string} fpath
+ * @param {Buffer} fdata
+ */
   assertPages(fpath, fdata) {
+    // Parse the lines of JSON data
     const lines = Buffer.from(fdata).toString().split('\n').map(JSON.parse);
+    // Get the last entry to avoid the first line header
     const entry = lines[lines.length - 1];
-    if(!entry.url || !entry.ts) {
-      throw `${fpath}: must be a valid JSON-Lines file with url and ts properties`;
+    // Both `url` and `ts` are required per the spec
+    if(!entry.url) {
+      throw `${fpath}: must be a valid JSON-Lines file with url properties`;
+    } else if(!entry.ts) {
+      throw `${fpath}: must be a valid JSON-Lines file with ts properties`;
     }
   }
 
-  _filesProxy;
-
-  get files() {
-    if(!this._filesProxy){
-      this.files = {}// initialize proxy
-    }
-    return this._filesProxy;
-  }
-
-  set files(obj) {
-    Object.entries(obj).forEach(([fpath, fdata]) => {
-      return this.validateFile(fpath, fdata);
-    })
-
-    this._filesProxy = new Proxy(obj, {
-      set: (target, fpath, fdata) => {
-        this.validateFile(fpath, fdata);
-        target[fpath] = fdata;
-        return true;
-      }
-    });
-  }
-
+/**
+ * Given a file path, find validations that should apply
+ * and run then against the data
+ *
+ * @param {string} fpath
+ * @param {Buffer} fdata
+ */
   validateFile(fpath, fdata) {
     this.validations
         .filter(([regex]) => regex.test(fpath))
@@ -84,6 +110,44 @@ export class WACZ {
 
   datapackage = {};
 
+  _filesProxy;
+
+  get files() {
+    if(!this._filesProxy){
+      // initialize the proxy on first access
+      // if it hasn't already been created
+      this.files = {}
+    }
+    return this._filesProxy;
+  }
+
+  /**
+   * Use a Proxy to validate any entries added to the `files` property
+   */
+  set files(obj) {
+    // validate all properties on initial assignment
+    Object.entries(obj).forEach(([fpath, fdata]) => {
+      return this.validateFile(fpath, fdata);
+    })
+
+    // create a new proxy to handle additional assignments to `files`
+    this._filesProxy = new Proxy(obj, {
+      set: (target, fpath, fdata) => {
+        this.validateFile(fpath, fdata);
+        target[fpath] = fdata;
+        return true;
+      }
+    });
+  }
+
+  /**
+   * Generate a CDXJ index based on a list of files.
+   * Defaults to using `this.files` but since the spec
+   * allows multiple indexes, this function can be used
+   * to generate against any set of files
+   *
+   * @param {Object} files - an object whose keys are the file paths and values are the file data
+   */
   async generateIndexCDX(files = this.files) {
     const buffers = [];
     const converter = new Writable();
