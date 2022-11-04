6d31f006c0410497f53d6a17571c579e16aa48e5
Greg Leppert <greg@leppert.me>
2022-11-04 11:26:42 -0400

    Document waczToMischief importer

--- importers/waczToMischief.js
+++ importers/waczToMischief.js
@@ -3,28 +3,49 @@ import StreamZip from "node-stream-zip";
 import { Mischief } from "../Mischief.js";
 import { MischiefProxyExchange } from "../exchanges/MischiefProxyExchange.js";
 
+/**
+ * Reconstructs a Mischief capture from a WACZ
+ * containing raw http traffic data.
+ *
+ * @param {string} zipPath - path to the zipped WACZ
+ * @returns {Promise<Mischief>} - a reconstructred Mischief capture object
+ */
 export async function waczToMischief(zipPath) {
   const zip = new StreamZip.async({ file: zipPath });
-  const entries = await zip.entries();
   const pageJSON = await getPageJSON(zip);
 
   const capture = new Mischief(pageJSON.url);
   Object.assign(capture, {
     id: pageJSON.id,
     startedAt: new Date(pageJSON.ts),
-    exchanges: await getExchanges(zip, entries)
+    exchanges: await getExchanges(zip)
   })
 
   await zip.close();
   return capture;
 }
 
+/**
+ * Retrieves the pages.jsonl data from the WARC and parses it
+ *
+ * @param {StreamZipAsync} zip
+ * @returns {object[]} - an array of page entry objects
+ */
 const getPageJSON = async (zip) => {
   const data = await zip.entryData('pages/pages.jsonl');
   return data.toString().split('\n').map(JSON.parse)[1];
 }
 
+
+/**
+ * Retrieves the raw requests and responses and initializes
+ * them into MischiefProxyExchanges
+ *
+ * @param {StreamZipAsync} zip
+ * @returns {MischiefProxyExchange[]} - an array of reconstructed MischiefProxyExchanges
+ */
 const getExchanges = async (zip, entries) => {
+  const entries = await zip.entries();
   const props = await Promise.all(
     Object.values(entries)
     // only entries in the raw folder
