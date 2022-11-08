a38d7d59daba1de6e2e55b7480d4c98551c462ba
Greg Leppert <greg@leppert.me>
2022-11-08 09:22:57 -0500

    Rename wacz fixture

--- test/importers/waczToMischief.js
+++ test/importers/waczToMischief.js
@@ -4,6 +4,6 @@ import assert from 'node:assert/strict';
 import { waczToMischief } from '../../importers/waczToMischief.js'
 
 test('exchanges should be rehydrated', async (_t) => {
-  const mischief = await waczToMischief('test/fixtures/example-https.HDwacz');
+  const mischief = await waczToMischief('test/fixtures/example-https-with-raw.wacz');
   assert.strictEqual(mischief.exchanges.length, 2);
 })
