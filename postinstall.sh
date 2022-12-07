b6866158945eeba12f3ee3a3acfa0dbca6cb50fc
Matteo Cargnelutti <mcargnelutti@law.harvard.edu>
2022-12-07 12:23:06 +0100

    Update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,11 +1,7 @@
 #-------------------------------------------------------------------------------
 # Post-install script
 #-------------------------------------------------------------------------------
-
 # Pull yt-dlp (2022.11.11 version)
 mkdir ./executables/;
 curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2022.11.11/yt-dlp > ./executables/yt-dlp;
 chmod a+x ./executables/yt-dlp;
-
-# Create tmp directory
-mkdir ./tmp/;
