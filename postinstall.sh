92a3b104be8035d3c0255504954551a25100a631
Matteo Cargnelutti <mcargnelutti@law.harvard.edu>
2023-01-03 12:28:11 -0500

    Dependencies update

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # Post-install script
 #-------------------------------------------------------------------------------
-# Pull yt-dlp (2022.11.11 version)
+# Pull yt-dlp (2023.01.02 version)
 mkdir ./executables/;
-curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2022.11.11/yt-dlp > ./executables/yt-dlp;
+curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2023.01.02/yt-dlp > ./executables/yt-dlp;
 chmod a+x ./executables/yt-dlp;
