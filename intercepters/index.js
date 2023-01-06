dae860689ba5220e37bf734f785ab141b70e8092
Greg Leppert <greg@leppert.me>
2023-01-06 14:31:39 -0500

    Typo

--- intercepters/index.js
+++ intercepters/index.js
@@ -1,7 +1,7 @@
 /**
  * Mischief
  * @module intercepters
- * @description Entry point for the scribes module.
+ * @description Entry point for the intercepters module.
  */
 import { MischiefIntercepter } from './MischiefIntercepter.js'
 import { MischiefProxy } from './MischiefProxy.js'
