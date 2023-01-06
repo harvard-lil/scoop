e591cc42bd23a9955ce481d0c04abb76aa976c61
Greg Leppert <greg@leppert.me>
2023-01-06 05:59:43 -0500

    Document constants

--- constants.js
+++ constants.js
@@ -1,13 +1,41 @@
+/**
+ * Mischief
+ * @module CONSTANTS
+ * @author The Harvard Library Innovation Lab
+ * @license MIT
+ * @description Constants used across the library.
+ */
+
 import chalk from 'chalk'
 
+/**
+ * Description of this software
+ */
 export const SOFTWARE = 'Mischief @ Harvard Library Innovation Lab'
 
+/**
+ * The current version of Mischief
+ */
 export const VERSION = 'v0.0.1 DEV'
 
+/**
+ * The version of WARC this library exports
+ */
 export const WARC_VERSION = '1.1'
 
+/**
+ * The version of WACZ this library exports
+ */
 export const WACZ_VERSION = '1.1.1'
 
+/**
+ * Location of the directory in which assets may be rendered (ex: the provinance summary)
+ */
+export const ASSETS_DIR = './assets/'
+
+/**
+ * Colors used by the logging function
+ */
 export const LOGGING_COLORS = {
   DEFAULT: chalk.gray,
   TRACE: chalk.magenta,
@@ -16,5 +44,3 @@ export const LOGGING_COLORS = {
   WARN: chalk.yellow,
   ERROR: chalk.red
 }
-
-export const ASSETS_DIR = './assets/'
