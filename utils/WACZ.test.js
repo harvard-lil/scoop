6bdd9f171faeec915f983d731e2da73545e21974
Greg Leppert <greg@leppert.me>
2023-01-27 10:57:22 -0500

    Simplify test

--- utils/WACZ.test.js
+++ utils/WACZ.test.js
@@ -1,30 +1,28 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
 
-import { readFile } from 'fs/promises'
 import AdmZip from 'adm-zip'
 
 import { BASE_PATH } from '../constants.js'
 import { WACZ } from './WACZ.js'
 
-const warc = await readFile(`${BASE_PATH}/fixtures/example-https.warc`)
-const wacz = new AdmZip(`${BASE_PATH}/fixtures/example-https.wacz`)
+const exampleFixtureZip = new AdmZip(`${BASE_PATH}/fixtures/example-https.wacz`)
+const exampleFixtureEntries = exampleFixtureZip.getEntries().map(entry => [entry.entryName, exampleFixtureZip.readFile(entry)])
+const fixture = Object.fromEntries(exampleFixtureEntries)
 
 test('WACZ should validate WARC files', async (_t) => {
   assert.doesNotThrow(() => {
-    new WACZ({ files: {'archives/archive.warc': warc} }) // eslint-disable-line
+    new WACZ({ files: {'archive/data.warc': fixture['archive/data.warc']} }) // eslint-disable-line
   })
 
   assert.throws(() => {
-    new WACZ({ files: {'archives/archive.warc': Buffer.from([1, 2, 3])} }) // eslint-disable-line
+    new WACZ({ files: {'archive/data.warc': Buffer.from([1, 2, 3])} }) // eslint-disable-line
   })
 })
 
 test('WACZ should validate index files', async (_t) => {
-  const file = wacz.readFile(wacz.getEntry('indexes/index.cdx'))
-
   assert.doesNotThrow(() => {
-    new WACZ({ files: {'indexes/index.cdx': file} }) // eslint-disable-line
+    new WACZ({ files: {'indexes/index.cdx': fixture['indexes/index.cdx']} }) // eslint-disable-line
   })
 
   assert.throws(() => {
@@ -33,13 +31,18 @@ test('WACZ should validate index files', async (_t) => {
 })
 
 test('WACZ should validate pages files', async (_t) => {
-  const file = wacz.readFile(wacz.getEntry('pages/pages.jsonl'))
-
   assert.doesNotThrow(() => {
-    new WACZ({ files: {'pages/pages.jsonl': file} }) // eslint-disable-line
+    new WACZ({ files: {'pages/pages.jsonl': fixture['pages/pages.jsonl']} }) // eslint-disable-line
   })
 
   assert.throws(() => {
     new WACZ({ files: {'pages/pages.jsonl': Buffer.from([1, 2, 3])} }) // eslint-disable-line
   })
+})
+
+test('WACZ should generate a valid index file', async (_t) => {
+  const wacz = new WACZ({ files: fixture })
+  const index = await wacz.generateIndexCDX()
+
+  assert.deepEqual(index, fixture['indexes/index.cdx'])
 })
