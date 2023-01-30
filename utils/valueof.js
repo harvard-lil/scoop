b23537585b83c806abfccccf88c1ae9b029632ab
Greg Leppert <greg@leppert.me>
2023-01-30 17:16:17 -0500

    Add a comment to valueof

--- utils/valueof.js
+++ utils/valueof.js
@@ -2,6 +2,19 @@ import { Mischief } from '../Mischief.js'
 import { MischiefProxyExchange } from '../exchanges/MischiefProxyExchange.js'
 import { MischiefGeneratedExchange } from '../exchanges/MischiefGeneratedExchange.js'
 
+/**
+ * A utility function for testing purposes that converts
+ * a value into a primative version for easier comparison
+ * against similar values.
+ *
+ * For example, comparing an original Mischief capture against
+ * a rehydrated version populated by a WACZ import. In this case, certain
+ * transient internal properties may not match exactly but the substantive
+ * properties match.
+ *
+ * @param {any} source - A value to be converted into a primative version
+ * @returns {any} A primative version of the supplied source
+ */
 export function valueOf (source) {
   switch (source.constructor) {
     case Array: {
