0a8fa41b82a63e12e546732b0d633e304f17fcdf
Greg Leppert <greg@leppert.me>
2023-02-12 14:53:02 -0500

    Add docs to png.js

--- utils/png.js
+++ utils/png.js
@@ -7,10 +7,9 @@
 
 const magicNumber = Buffer.from([0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A])
 const IHDR = Buffer.from('IHDR')
-/*
- *
- * Checks the header of a buffer to see if it contains
- * the PNG magic number
+
+/**
+ * Checks a buffer to see if it contains the PNG magic number
  *
  * @param {Buffer} buffer - The buffer to check
  * @returns {boolean} True if buffer appears to be a png file.
@@ -20,7 +19,10 @@ export function isPNG (buffer) {
 }
 
 /**
+ * Checks a buffer to see if it contains an IHDR data field
  *
+ * @param {Buffer} buffer - The buffer to check
+ * @returns {boolean} True if buffer contains the IHDR data field
  */
 export function hasIHDR (buffer) {
   // The first four bytes represent the length of the chunk's data field and the following four bytes represent the chunk's name
@@ -28,6 +30,12 @@ export function hasIHDR (buffer) {
   return Buffer.compare(buffer.subarray(offset, offset + IHDR.length), IHDR) === 0
 }
 
+/**
+ * Extracts the width and height from a buffer containing PNG data
+ *
+ * @param {Buffer} buffer - The png data
+ * @returns {[width: integer, height: integer]} True if buffer contains the IHDR data field
+ */
 export function getDimensions (buffer) {
   if (!isPNG(buffer) || !hasIHDR(buffer)) {
     throw new Error('buffer must contain PNG data.')
