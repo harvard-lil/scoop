31c19970e9366088d087c53df4573fb027bc3e05
Matteo Cargnelutti <mcargnelutti@law.harvard.edu>
2023-03-06 14:43:42 -0500

    yt-dlp upgrade

--- postinstall.sh
+++ postinstall.sh
@@ -1,7 +1,7 @@
 #-------------------------------------------------------------------------------
 # Post-install script
 #-------------------------------------------------------------------------------
-# Pull yt-dlp (2023.02.17 version)
+# Pull yt-dlp (2023.03.04 version)
 mkdir ./executables/;
-curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2023.02.17/yt-dlp > ./executables/yt-dlp;
+curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2023.03.04/yt-dlp > ./executables/yt-dlp;
 chmod a+x ./executables/yt-dlp;
