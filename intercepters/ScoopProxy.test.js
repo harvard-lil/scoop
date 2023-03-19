b818846e1561c7b1b2c5f1e9050f0eaef337132d
Greg Leppert <greg@leppert.me>
2023-03-19 06:49:46 -0400

    Rework test to match new syntax

--- intercepters/ScoopProxy.test.js
+++ intercepters/ScoopProxy.test.js
@@ -1,7 +1,7 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
+import { PassThrough } from 'node:stream'
 
-import Session from 'transparent-proxy/core/Session.js'
 import detectPort from 'detect-port'
 
 import { ScoopProxy } from './index.js'
@@ -13,27 +13,6 @@ const BLOCKLISTED_IP = '127.0.0.1'
 const BLOCKLISTED_URL = 'http://localhost'
 const NON_BLOCKLISTED_IP = '93.184.216.34'
 const NON_BLOCKLISTED_URL = 'https://lil.law.harvard.edu'
-
-/**
- * Creates a mock transparent-proxy Session object.
- * @param {number} id
- * @param {string} ip
- * @param {string} url
- * @returns {Session}
- * @ignore
- */
-function mockSession (id, ip, url) {
-  const session = new Session(id)
-  session._dst = { remoteAddress: ip }
-  session._src = { destroyed: false }
-  session.request.path = url
-
-  session.destroy = () => {
-    session._src.destroyed = true
-  }
-
-  return session
-}
 
 test('ScoopProxy starts and stops a proxy on the requested port', async (_t) => {
   const capture = new Scoop(NON_BLOCKLISTED_URL, testDefaults)
@@ -62,7 +41,7 @@ test('contextOptions returns proxy information in a format that can be directly 
   assert.equal(contextOptions.proxy.server, `http://${testDefaults.proxyHost}:${testDefaults.proxyPort}`)
 })
 
-test('checkRequestAgainstBlocklist should detect and interrupt blocklisted exchanges.', async (_t) => {
+test('foundInBlocklist should detect and interrupt blocklisted exchanges.', async (_t) => {
   const capture = new Scoop(NON_BLOCKLISTED_URL, testDefaults)
   const intercepter = capture.intercepter
 
@@ -73,19 +52,22 @@ test('checkRequestAgainstBlocklist should detect and interrupt blocklisted excha
 
   for (const scenario of scenarios) {
     const { path, remoteAddress, shouldBeInterrupted } = scenario
-    const session = mockSession(12, remoteAddress, path)
+    const request = { socket: new PassThrough() }
 
-    assert.equal(intercepter.checkRequestAgainstBlocklist(session), shouldBeInterrupted)
-    assert.equal(session._src.destroyed, shouldBeInterrupted)
+    assert(!request.socket.destroyed)
+    assert.equal(
+      intercepter.foundInBlocklist(path, request) || intercepter.foundInBlocklist(remoteAddress, request),
+      shouldBeInterrupted
+    )
+    assert.equal(request.socket.destroyed, shouldBeInterrupted)
   }
 })
 
-test('interceptRequest returns undefined when trying to intercept a session for a blocklisted exchange.', async (_t) => {
+test('intercept returns undefined when no matching exchange with provided request is not found due to blocklisting.', async (_t) => {
   const capture = new Scoop(NON_BLOCKLISTED_URL, testDefaults)
   const intercepter = capture.intercepter
-  const session = mockSession(12, BLOCKLISTED_IP, BLOCKLISTED_URL)
 
-  assert.equal(intercepter.interceptRequest(Buffer.from(''), session), undefined)
+  assert.equal(intercepter.intercept('request', Buffer.from(''), {}), undefined)
 })
 
 test('recordExchanges flag actively controls whether records are added to exchanges list.', async (_t) => {
@@ -93,20 +75,16 @@ test('recordExchanges flag actively controls whether records are added to exchan
   const intercepter = capture.intercepter
 
   const scenarios = [
-    { id: 12, recordExchanges: true, expectedExchangesLength: 1 },
-    { id: 13, recordExchanges: false, expectedExchangesLength: 1 },
-    { id: 14, recordExchanges: true, expectedExchangesLength: 2 }
+    { recordExchanges: true, expectedExchangesLength: 1 },
+    { recordExchanges: false, expectedExchangesLength: 1 },
+    { recordExchanges: true, expectedExchangesLength: 2 }
   ]
 
   for (const scenario of scenarios) {
-    const { id, recordExchanges, expectedExchangesLength } = scenario
-    const session = mockSession(id, NON_BLOCKLISTED_IP, NON_BLOCKLISTED_URL)
-
-    const data = Buffer.from('')
-
+    const { recordExchanges, expectedExchangesLength } = scenario
     intercepter.recordExchanges = recordExchanges
 
-    intercepter.intercept('response', data, session)
+    intercepter.onRequest(Object.assign(new PassThrough(), { url: '' }))
     assert.equal(intercepter.exchanges.length, expectedExchangesLength)
   }
 })
