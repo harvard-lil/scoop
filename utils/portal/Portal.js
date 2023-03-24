f83e9449e434f5a2ba2232f09f4d6de0e6a47a1c
Greg Leppert <greg@leppert.me>
2023-03-24 09:31:40 -0400

    Properly release sockets from http.request

--- utils/portal/Portal.js
+++ utils/portal/Portal.js
@@ -65,7 +65,52 @@ function getServerDefaults (request) {
   }
 }
 
-const CRLFx2 = '\r\n\r\n'
+const CRLF = '\r\n'
+
+function releaseSocket (req) {
+  const { socket } = req
+
+  /**
+   * A listener must be present or the socket will be closed
+   * @see {@link https://nodejs.org/api/http.html#event-upgrade}
+   * @see {@link https://github.com/nodejs/node/blob/c5881458106487f80d31513096b4d0baa88828b8/lib/_http_client.js#L564}
+   */
+  req.on('upgrade', () => {})
+
+  /**
+   * emit a fake switching response to trigger the
+   * release of the socket and parser from the request
+   * NOTE: the `Upgrade` and `Connection` headers are required for the parser to flip the `upgrade` flag
+   * @see {@link https://github.com/nodejs/node/blob/c5881458106487f80d31513096b4d0baa88828b8/lib/_http_client.js#L545}
+   */
+  socket.emit('data', Buffer.from([
+    'HTTP/1.1 101 Switching Protocols',
+    'Upgrade: shim',
+    'Connection: Upgrade',
+    CRLF
+  ].join(CRLF)))
+
+  /**
+   * The upgrade logic also removes the socket from the agent pool
+   * with the idea that you'll have a long-running websocket attached
+   * so we must add it back to the pool by temporarily patching createConnection.
+   * Despite the name, `createSocket` does not create the socket, it defers to `createConnection` for that.
+   * Instead, it attaches listeners and inserts the socket into the pool so we're forcing
+   * `createConnection` to just return the socket we already have.
+   * @see {@link https://github.com/nodejs/node/blob/c5881458106487f80d31513096b4d0baa88828b8/lib/_http_client.js#L568}
+   * @see {@link https://github.com/nodejs/node/blob/c5881458106487f80d31513096b4d0baa88828b8/lib/_http_agent.js#L311}
+   * @see {@link https://github.com/nodejs/node/blob/c5881458106487f80d31513096b4d0baa88828b8/lib/_http_agent.js#L334}
+   */
+  const createConnection = req.agent.createConnection
+  try {
+    req.agent.createConnection = (...args) => args[0]?.socket
+      ? args[0].socket
+      : createConnection(...args)
+    req.agent.createSocket(null, { socket, servername: 'bypass' }, () => {})
+  } finally {
+    req.agent.createConnection = createConnection
+  }
+}
 
 function getHandler (proxy, clientOptions, serverOptions, requestTransformer, responseTransformer) {
   return async (clientRequest, _, head) => {
@@ -95,14 +140,25 @@ function getHandler (proxy, clientOptions, serverOptions, requestTransformer, re
 
           // Letting client know we've made the connection @see {@link https://reqbin.com/Article/HttpConnect}
           // TODO: CONNECT is hop-by-hop and we should handle additional hops down the line
-          clientSocket.write('HTTP/1.1 200 Connection Established' + CRLFx2)
+          clientSocket.write(['HTTP/1.1 200 Connection Established', CRLF].join(CRLF))
           serverSocket.write(head)
+          releaseSocket(serverRequest)
         } else {
           clientSocket.mirror.pipe(requestTransformer(clientRequest)).pipe(serverSocket)
         }
       }))
       .on('response', (serverResponse) => {
         const { socket: serverSocket } = serverResponse
+
+        /**
+         * Must be called to release the socket back into the agent pool.
+         * Needed since we never call `req.end()` and instead just pipe data through the socket.
+         * Node's `res.on('end')` handler will set `req._ended = true` which the
+         * `req.on('finish')` handler uses to determine whether to send the socket back to the pool.
+         * @see {@link https://github.com/nodejs/node/blob/c5881458106487f80d31513096b4d0baa88828b8/lib/_http_client.js#L748}
+         * @see {@link https://github.com/nodejs/node/blob/c5881458106487f80d31513096b4d0baa88828b8/lib/_http_client.js#L786}
+         */
+        serverResponse.on('end', () => serverResponse.req.emit('finish'))
 
         // On response, forward the original server response on to the client
         // We're using on('data') at the end, instead of pipe, to avoid unnecessary listeners (ex: 'unpipe')
