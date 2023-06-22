763f94c79be955e4ffee6e6488b97baa7ee0152c
Matteo Cargnelutti <mcargnelutti@law.harvard.edu>
2023-06-22 10:19:19 -0400

    Upgrade to yt-dlp 2023.06.22
    Changelog:
    - https://github.com/yt-dlp/yt-dlp/releases/tag/2023.06.21
    - https://github.com/yt-dlp/yt-dlp/releases/tag/2023.06.22
    
    This latest version fixes the bug introduced in 2023.06.21 that prevented us from upgrading.

--- postinstall.sh
+++ postinstall.sh
@@ -3,8 +3,8 @@
 #-------------------------------------------------------------------------------
 mkdir ./executables/;
 
-# Pull yt-dlp (v2023.03.04)
-curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2023.03.04/yt-dlp > ./executables/yt-dlp;
+# Pull yt-dlp (v2023.06.22)
+curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2023.06.22/yt-dlp > ./executables/yt-dlp;
 chmod a+x ./executables/yt-dlp;
 
 # Pull crip (v2.1.0)
