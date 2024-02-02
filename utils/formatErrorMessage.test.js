5a9b431e25bd7fdd25ac385e80b5c83ddbd77fbf
Rebecca Cremona <rcremona@law.harvard.edu>
2024-02-02 18:46:49 -0500

    Lint.

--- utils/formatErrorMessage.test.js
+++ utils/formatErrorMessage.test.js
@@ -1,21 +1,21 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
 
-import {formatErrorMessage} from './formatErrorMessage.js'
+import { formatErrorMessage } from './formatErrorMessage.js'
 
 test('formatErrorMessage should strip colors', async (_t) => {
   try {
-    throw '\u001B[0m\u001B[4m\u001B[42m\u001B[31mfoo\u001B[39m\u001B[49m\u001B[24mfoo\u001B[0m';
+    throw '\u001B[0m\u001B[4m\u001B[42m\u001B[31mfoo\u001B[39m\u001B[49m\u001B[24mfoo\u001B[0m'
   } catch (err) {
-    assert(formatErrorMessage(err) == 'foofoo');
+    assert(formatErrorMessage(err) === 'foofoo')
   }
 })
 
 test('formatErrorMessage should truncate length', async (_t) => {
   try {
-    throw 'a'.repeat(200);
+    throw 'a'.repeat(200)
   } catch (err) {
-    assert(formatErrorMessage(err) == 'a'.repeat(100) + '...')
+    assert(formatErrorMessage(err) === 'a'.repeat(100) + '...')
   }
 })
 
@@ -39,30 +39,30 @@ test('formatErrorMessage should strip block formatting', async (_t) => {
   try {
     throw actualErrorMessage
   } catch (err) {
-    assert(formatErrorMessage(err) == 'browserType.launch: Looks like you launched a headed browser without having a XServer running. Set e...');
+    assert(formatErrorMessage(err) === 'browserType.launch: Looks like you launched a headed browser without having a XServer running. Set e...')
   }
 })
 
 test('formatErrorMessage handles arbitrary object', async (_t) => {
   try {
-    throw NaN;
+    throw NaN
   } catch (err) {
-   formatErrorMessage(err);
+    formatErrorMessage(err)
   }
 })
 
 test('formatErrorMessage handles string', async (_t) => {
   try {
-    throw 'hi';
+    throw 'hi'
   } catch (err) {
-   assert(formatErrorMessage(err) == 'hi');
+    assert(formatErrorMessage(err) === 'hi')
   }
 })
 
 test('formatErrorMessage handles exception', async (_t) => {
   try {
-    throw new Error ('hi');
+    throw new Error('hi')
   } catch (err) {
-   assert(formatErrorMessage(err) == 'hi');
+    assert(formatErrorMessage(err) === 'hi')
   }
 })
