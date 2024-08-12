5e4093aa8a9b8c9f549cc333801d0d2eaa85c0c5
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2024-08-12 11:20:33 -0700

    yt-dlp 2024.08.06

--- postinstall.sh
+++ postinstall.sh
@@ -3,8 +3,8 @@
 #-------------------------------------------------------------------------------
 mkdir ./executables/;
 
-# Pull yt-dlp (v2024.08.01)
-curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2024.08.01/yt-dlp > ./executables/yt-dlp;
+# Pull yt-dlp (v2024.08.06)
+curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2024.08.06/yt-dlp > ./executables/yt-dlp;
 chmod a+x ./executables/yt-dlp;
 
 # Pull crip (v2.1.0)
