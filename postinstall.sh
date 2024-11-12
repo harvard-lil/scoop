1113f472e8de667759cd3c0406dc38326e831e9d
Ebru Yucesar <eyucesar@law.harvard.edu>
2024-11-12 11:14:46 -0500

    upgrade yt-dlp

--- postinstall.sh
+++ postinstall.sh
@@ -3,8 +3,8 @@
 #-------------------------------------------------------------------------------
 mkdir ./executables/;
 
-# Pull yt-dlp (v2024.10.22)
-curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2024.10.22/yt-dlp > ./executables/yt-dlp;
+# Pull yt-dlp (v2024.11.04)
+curl -L https://github.com/yt-dlp/yt-dlp/releases/download/2024.11.04/yt-dlp > ./executables/yt-dlp;
 chmod a+x ./executables/yt-dlp;
 
 # Pull crip (v2.1.0)
